57c2804b5c6b6cdafe1e12ac30bd68a35fa138a4
Henry de Valence <hdevalence@penumbralabs.xyz>
2024-10-29 21:01:48 -0700

    wip

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "usb": false,
+      "bufferutil": false,
+      "utf-8-validate": false,
+    };
+    return config;
+  },
+}
 
-export default nextConfig;
+export default nextConfig
